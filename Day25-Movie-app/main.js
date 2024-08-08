4e3c0f253b37cfe3ed768c68ce1df946a38f17b6
Vikas singh varma <Vikashverma3001@gmail.com>
2024-08-09 00:09:14 +0530

    movie app

--- Day25-Movie-app/main.js
+++ Day25-Movie-app/main.js
@@ -14,39 +14,36 @@ async function getMovieInfo(query){
     }
 }
 
-// show more info movie function
-async function showMoreInfo(showId){
-    try {
-        const res = await fetch(`https://api.tvmaze.com/shows/${showId}?embed=cast`)
-        const result = await res.json()
-        console.log(result)
-        displayMovieInfo(result);
-        return;
-    } catch (error) {
-        console.log(error.message)
+    // show more info movie function
+    async function showMoreInfo(showId){
+        try {
+            const res = await fetch(`https://api.tvmaze.com/shows/${showId}?embed=cast`)
+            const result = await res.json()
+            console.log(result)
+            displayMovieInfo(result);
+            return;
+        } catch (error) {
+            console.log(error.message)
+        }
     }
-}
-
 
 // movie details show function
 function showIncontainer(movies){
     for (const movie of movies) {
         let title = movie.show?.name;
         let poster = movie.show?.image?.medium;
-        let realeseYear = movie.show?.
-premiered;
+        let realeseYear = movie.show?.premiered;
         let rating = movie.show.rating.average;
         let id = movie.show?.id
-        console.log(id)
+        // console.log(id)
 
         //  <span><a href="#movie-detail" id ="moreInfo">More-info</a></span>
         const div = document.createElement('div');
-        div.classList.add = "movie-details";
+        div.classList.add("movie-details");
         div.innerHTML = `
             <img class="movie-poster" src="${poster}" alt="Movie Poster">
-            <div class = "movie-some-info">
-                <h4>${title} <span id="more-info-btn" onclick="showMoreInfo(${id})">More-Info</span></h4>
-                
+            <div class = "movie-some-info"> 
+            <h4>${title} <span class="more-info-btn" data-id="${id}">More-Info</span></h4>    
                 <span><strong>Release Year:</strong> ${realeseYear}</span>
                 <span><strong>Rating:</strong> ${rating}</span>
             </div>
@@ -55,6 +52,13 @@ premiered;
     }
 }
 
+ 
+container.addEventListener('click', function(e) {
+    if (e.target && e.target.classList.contains('more-info-btn')) {
+        const showId = e.target.getAttribute('data-id');
+        showMoreInfo(showId);
+    }
+});
 
 // display movie info
 function displayMovieInfo(movieInfo){
@@ -67,7 +71,7 @@ form.addEventListener('submit', async function(e){
         alert('Please provide movies or show name!')
     }
     const result = await getMovieInfo(input.value);
-    console.log(result);
+    // console.log(result);
     showIncontainer(result)
 })
 
